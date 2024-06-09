dcb0268b7e746ed510127ebea88a956b3e217bd6
Luis Soto <alejandrosotoserafin@gmail.com>
2024-06-09 17:02:31 -0600

    Day6ejercicios

---
+++ Sesiones Asincronas/1.JavaScript/Day6/1.consoleLog/ejercicios/51.CodingExercises.js
@@ -0,0 +1,5 @@
+// Dentro de la función calcular, imprimir en consola el resultado de sumar los 
+// dos números que recibe por parámetro.
+function calcular(num1, num2) {
+    console.log(num1 + num2);
+}
---
+++ Sesiones Asincronas/1.JavaScript/Day6/4.doWhile/ejercicios/56.CodingExercises.js
@@ -0,0 +1,16 @@
+// Implemente la función testLoop, donde debe utilizar un Loop Do While que 
+// itere tantas veces como lo indique el parámetro vueltas y que imprima en 
+// la consola en cada iteración el número de vuelta de la siguiente forma: 
+// Vuelta 0, Vuelta 1, etc. Utilice la variable contador para ir contando la 
+// cantidad de vueltas que va dando el ciclo.
+
+function testLoop(vueltas){
+    let contador = 0;
+    do{
+        console.log('Vuelta ' + contador);
+        contador ++;
+    }
+    while(contador <= vueltas);
+}
+
+testLoop(10)
