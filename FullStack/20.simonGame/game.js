c68ad70936a0c1443a0e6e2fe1fe481304d36895
Luis Soto <alejandrosotoserafin@gmail.com>
2024-11-16 22:43:09 -0600

    section20_last_part

--- FullStack/20.simonGame/game.js
+++ FullStack/20.simonGame/game.js
@@ -4,18 +4,28 @@ let buttonColours = ['red', 'blue', 'green', 'yellow'];
 
 let userClickedPattern = [];
 
+let level = 0;
+
+let isGameStarted = false;
+
+$(document).keypress(function() {
+    if(isGameStarted == false){
+        nextSequence();
+        isGameStarted = true;
+    };
+});
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random()*4);
-
     let randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
 
     let buttonSelected = $(`#${randomChosenColour}`);
-
     buttonSelected.fadeIn(100).fadeOut(100).fadeIn(100);
-
     playSound(randomChosenColour);
+
+    $("h1").text(`Level ${level}`);
+    level += 1;
 };
 
 let buttons = $('div .btn').click(function(event) {
@@ -26,6 +36,8 @@ let buttons = $('div .btn').click(function(event) {
 
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length -1);
 });
 
 function playSound(name){
@@ -33,14 +45,39 @@ function playSound(name){
     sound.play();
 };
 
-// 1. Create a new function called animatePress(), it should take a single input 
-//parameter called currentColour.
 function animatePress(currentColour){
     $(`div #${currentColour}`).addClass("pressed")
-    setTimeout(function(event){
+    setTimeout(function(){
         $(`div #${currentColour}`).removeClass("pressed");
     }, 100);
 };
 
-// 2. Take a look inside the styles.css file, you can see there is a class called "pressed", 
-//it will add a box shadow and changes the background colour to grey.
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+        if(currentLevel === gamePattern.length -1){
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+            userClickedPattern = [];
+        }
+    }
+    else {
+        console.log("wrong");
+        let wrongSound = new Audio("./sounds/wrong.mp3");
+        wrongSound.play();
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        },200);
+        $("h1").text("Game Over. Press any key to restart");
+        startOver();
+    };
+};
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+    isGameStarted = false;
+};
