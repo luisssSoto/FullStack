53d92ad0b6742631b572e4150c24ff0d9f0adc6c
AlexSoto <alejandrosotoserafin@gmail.com>
2024-11-20 22:11:18 -0600

    section22_last_part

--- FullStack/21.UnixCommandLine/test.js
+++ FullStack/21.UnixCommandLine/test.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var gender = String;
+var gender = process.argv[2];
 console.log(`Gender: ${gender}`);
 
-var areYouHuman = Boolean;
+var areYouHuman = process.argv[3];
 console.log(`Are you human: ${areYouHuman}`);
 
-let password = '';
+let password = process.argv[4];
 if(password === "contraseña"){
     console.log('Come in');
 }
