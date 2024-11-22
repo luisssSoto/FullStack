a33c870786b47944395471b89332d2f63ea1a613
Luis Soto <alejandrosotoserafin@gmail.com>
2024-11-22 06:16:15 -0600

    section23_first_part

---
+++ FullStack/23.NodeJS/195.UsingNode.js
@@ -0,0 +1,23 @@
+"use strict";
+//Node REPL -> Read Eval Print Loop
+//It is a computer environment where user inputs are read and evaluated,
+//and then the results are returned to the user
+
+//In command line type:
+//node --version to know what is the exactly version you have 
+
+//To initiate node REPL:
+//node
+
+//To see some commands:
+//.help
+
+//You can type any JS sentence in node REPL and wait for the response
+
+//To exit from node REPL:
+//.exit or CTRL+C
+
+//Running files:
+//node nameFile.js
+
+console.log("Hello from Node.js");
